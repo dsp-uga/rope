b1f107b8b6f5c9ce7ed0ae9be9a61c0121da120d
Omid <os14526@uga.edu>
2018-04-17 10:55:53 -0400

    adds path variables to data processor

--- data/creating_numpy.py
+++ data/creating_numpy.py
@@ -3,7 +3,11 @@ import os
 import numpy as np 
 import csv
 import cv2
-a=glob.glob('/home/rdey/dsp_final/train/*.jpg')
+
+train_path = 'train/'
+output_path = 'train_np/'
+
+a=glob.glob( os.path.join( train_path, '*.jpg'))
 X_train=[]
 y_train=[]
 counter=0
@@ -12,6 +16,9 @@ csv_file_1=dict((rows[0],rows[2])for rows in csv_file)
 csv_file=csv_file_1
 #print(csv_file[0])
 #print(a[0].replace('/home/rdey/dsp_final/train/','').replace('.jpg',''))
+
+
+
 counter=0
 to_skip=0
 for i in range (0,len(a)):
@@ -21,21 +28,21 @@ for i in range (0,len(a)):
     try:
 
         #print(('/home/rdey/dsp_final/train/'+str(a[i].replace('/home/rdey/dsp_final/train/','').strip())))
-        temp_x=cv2.imread(('/home/rdey/dsp_final/train/'+str(a[i].replace('/home/rdey/dsp_final/train/','').strip())),1)
+        temp_x=cv2.imread((train_path+str(a[i].replace(train_path,'').strip())),1)
         
 
         try:
 
                 
-            y_train.append(csv_file[str(a[i].replace('/home/rdey/dsp_final/train/','').replace('.jpg',''))])
+            y_train.append(csv_file[str(a[i].replace(train_path,'').replace('.jpg',''))])
             X_train.append(cv2.resize(temp_x,(64,64)))
         except:
 
             print("cant find entry")
         
         if(i%10000==0 and to_skip==1):
-            np.save('/home/rdey/dsp_final/train_np/X_train'+str(counter)+'.npy',np.array(X_train))
-            np.save('/home/rdey/dsp_final/train_np/y_train'+str(counter)+'.npy',np.array(y_train))
+            np.save(  output_path+ '/X_train'+str(counter)+'.npy',np.array(X_train))
+            np.save(output_path+'/y_train'+str(counter)+'.npy',np.array(y_train))
             counter+=1
             X_train=[]
             y_train=[]
