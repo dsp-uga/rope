dc172296a5bd4afcb730522986e81301b45484cb
Omid <os14526@uga.edu>
2018-04-25 12:04:11 -0400

    updates the basic clustrering implemetation

--- src/models/clustering.py
+++ src/models/clustering.py
@@ -1,12 +1,22 @@
 from sklearn.cluster import KMeans
 import numpy as np
-X = np.array([[1, 2], [1, 4], [1, 0],
-               [4, 2], [4, 4], [4, 0]])
+from sklearn.externals import joblib
 
-kmeans = KMeans(n_clusters=2, random_state=0)\
 
-kmeans.fit(X)
-kmeans.labels_
+x_train_path ='/home/omid/teamRope/team-rope/data/train_np_90k/'
 
-kmeans.predict([[0, 0], [4, 4]])
-print(kmeans.cluster_centers_)
+X_train = np.load((x_train_path + "X_train1.npy"))
+
+X_train = X_train.reshape( X_train.shape[0], 64*64*3 )
+
+# y_train = np.load((x_train_path + "y_train" + str(i) + ".npy"))  # .reshape(X_train.shape)
+
+kmeans = KMeans(n_clusters=100, random_state=0)
+
+kmeans.fit(X_train)
+
+joblib.dump(kmeans, 'kmeans_100_Classes.pkl')
+# print(kmeans.labels_)
+
+# kmeans.predict([[0, 0], [4, 4]])
+# print(kmeans.cluster_centers_)
