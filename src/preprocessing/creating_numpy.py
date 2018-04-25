b0dfd5813d6bae33f8870184e37c7203ec5fe3c6
Omid <os14526@uga.edu>
2018-04-25 19:24:49 -0400

    fixes ordering issue

--- src/preprocessing/creating_numpy.py
+++ src/preprocessing/creating_numpy.py
@@ -7,7 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="", add_help='How to use', prog='python creating_numpy.py <options>')
 
-parser.add_argument("-if", "--inputfile", default="../data/tarin.csv",
+parser.add_argument("-if", "--inputfile", default=None,
                     help='Path to the CSV file [DEFAULT: "data/tarin.csv"]')
 
 parser.add_argument("-o", "--outputdir", default="../data/tarin_np/",
@@ -29,36 +29,39 @@ train_path = args.inputdir
 output_path = args.outputdir
 
 # get a list of all files
-a=glob.glob( os.path.join(train_path, '*.jpg'))
-X_train=[]
-y_train=[]
-counter=0
-csv_file=csv.reader(open(args.inputfile ),delimiter=',')
-csv_file_1=dict((rows[0],rows[2])for rows in csv_file)
-csv_file=csv_file_1
+file_list = sorted(glob.glob( os.path.join(train_path, '*.jpg')))
+X_train = []
+y_train = []
 
-counter=0
+if args.inputfile is not None:
+    csv_file = csv.reader(open(args.inputfile), delimiter=',')
+    csv_file_1 = dict((rows[0], rows[2])for rows in csv_file)
+    csv_file = csv_file_1
 
-records_per_file = int( args.skip )
-image_size = ( int( args.size ) ,int( args.size )  )
-for i in range (0,len(a)):
+counter = 0
+
+records_per_file = int(args.skip)
+image_size = (int(args.size), int(args.size))
+for i in range (0,len(file_list)):
     
-    if(i%100000==0 and i > 0):
+    if i % 100000 == 0 and i > 0:
         print("current=", i)
     try:
-
-        temp_x=cv2.imread((train_path+str(a[i].replace(train_path, '').strip())), 1)
+        temp_x = cv2.imread(file_list[i], 1)  #( (train_path+str(a[i].replace(train_path, '').strip())), 1)
 
         try:
-            y_train.append(csv_file[str(a[i].replace(train_path, '').replace('.jpg', ''))])
+            if args.inputfile is not None:
+                y_train.append(csv_file[os.path.splitext(os.path.basename(file_list[i]))[0]])
+
             X_train.append(cv2.resize(temp_x, image_size))
 
         except:
             print("cant find entry, skipping!")
         
         if i % records_per_file == 0 and i > 0:
-            np.save(output_path + '/X_train' + str(counter)+'.npy', np.array(X_train))
-            np.save(output_path + '/y_train' + str(counter)+'.npy', np.array(y_train))
+            np.save(output_path + '/X_' + str(counter)+'.npy', np.array(X_train))
+            if args.inputfile is not None:
+                np.save(output_path + '/y_' + str(counter)+'.npy', np.array(y_train))
             counter += 1
             X_train = []
             y_train = []
@@ -69,8 +72,9 @@ for i in range (0,len(a)):
 
 # save the last section
 if len(X_train) > 0:
-    np.save(output_path + '/X_train' + str(counter) + '.npy', np.array(X_train))
-    np.save(output_path + '/y_train' + str(counter) + '.npy', np.array(y_train))
+    np.save(output_path + '/X_' + str(counter) + '.npy', np.array(X_train))
+    if args.inputfile is not None:
+        np.save(output_path + '/y_' + str(counter) + '.npy', np.array(y_train))
 
 
 
